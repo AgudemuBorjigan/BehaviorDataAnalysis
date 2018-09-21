02cfab028cf5b3864103a4a4bf35e663bb5670e6
agudemuborjigan <agudemubor@gmail.com>
2018-09-21 17:25:50 -0400

    trivial

--- DataSorting.m
+++ DataSorting.m
@@ -4,11 +4,17 @@ subjs = {'S078', 'S117', 'S128', 'S132', 'S149', 'S123', 'S143', 'S084', 'S072',
 % 'S117DD' is not included
 
 %% EEG data in the order of subjects listed in subjs
+% ITC
 EEG_20us =  [0.08, 0.15, 0.06, -0.03, 0.06, 0.17, 0.34, 0.05, -0.01, 0.37, 0.01, 0.38, 0.01, 0.02, 0.11];
 EEG_60us =  [0.26, 1.03, 0.13,  0.38, 0.28, 0.33, 0.25, 0.12,  0.25, 0.15, 0.15, 0.28, 0.34, 0.03, 0.66];
 EEG_180us = [1.10, 1.39, 0.47,  0.87, 1.11, 0.68, 0.62, 0.73,  1.51, 0.56, 0.87, 1.31, 0.92, 0.32, 0.62];
 EEG_540us = [1.25, 1.27, 0.75,  1.04, 1.41, 1.22, 1.29, 0.73,  0.81, 1.22, 0.92, 1.16, 0.74, 0.41, 1.07];
 EEG_avg   = [0.50, 0.87, 0.21,  0.48, 0.58, 0.51, 0.43, 0.30,  0.55, 0.27, 0.43, 0.64, 0.47, 0.12, 0.57];
+% normalized magnitude
+EEG_mag20us  = [];
+EEG_mag60us  = [];
+EEG_mag180us = [];
+EEG_mag540us = [];
 %%
 fid = fopen('dataSet.csv', 'w');
 fprintf(fid, 'Subject, ITD, FMleft, FMright, 500Hzleft, 500Hzright, 4000Hzleft, 4000Hzright, block, EEG_20us, EEG_60us, EEG_180us, EEG_540us, EEG_avg\n');
