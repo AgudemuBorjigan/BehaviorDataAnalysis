702acfd6a74a463dee380e613c34a4d6eeff75e7
agudemuborjigan <agudemubor@gmail.com>
2019-06-16 14:13:02 -0400

    modified R

--- ITD_FM.R
+++ ITD_FM.R
@@ -8,19 +8,20 @@ library(car)
 library(ggplot2)
 
 dat$FMmedian <- (dat$FMleftMedian + dat$FMrightMedian)/2
+dat$FMmean <- (dat$FMleftMean + dat$FMrightMean)/2
 
 dat$HL500 <- (dat$HL500left + dat$HL500right)/2
 dat$HL500diff <- abs(dat$HL500left - dat$HL500right)
 
-dat$HL4K <- (dat$HL4Kleft + dat$HL4Kright)/2
-dat$HL4Kdiff <- abs(dat$HL4Kleft - dat$HL4Kright)
+dat$HFA <- (dat$HFAleft + dat$HFAright)/2
+dat$HFAdiff <- abs(dat$HFAleft - dat$HFAright)
 
 #m_behavior <- lm(ITDmax ~ FMmedian + HL500 + HL500diff + HL4K + HL4Kdiff, data = dat) 
-m_behavior <- lm(ITDmax ~ FMmedian, data = dat) 
-Anova(m_behavior)
+m_itd_fm <- lm(ITDmax ~ FMmedian, data = dat) 
+Anova(m_itd_fm)
 
-p_behavior <- ggplot(aes(x = FMmedian, y = ITDmax), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
-p_behavior + xlab('Fdev [dB re: 500 Hz]') + ylab('ITD [dB re: 1 us]') + annotate("text", x = 12, y = 44.5, size =8, label = paste("R = ", signif(sqrt(summary(m_behavior)$r.squared), 3),  " \nP =",signif(summary(m_behavior)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
+p_itd_fm <- ggplot(aes(x = FMmedian, y = ITDmax), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
+p_itd_fm + xlab('Fdev [dB re: 500 Hz]') + ylab('ITD [dB re: 1 us]') + annotate("text", x = 12, y = 44.5, size =8, label = paste("R = ", signif(sqrt(summary(m_itd_fm)$r.squared), 3),  " \nP =",signif(summary(m_itd_fm)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
 #ggtitle('ITD vs FM thresholds')
 
 m_itd_res <- lm(ITDmax ~ mistakeITD, data = dat)
@@ -34,9 +35,17 @@ dat$residFMright <- resid(m_fmRight_res) # adding residuals of the parameter to 
 
 dat$residFMmedian <- (dat$residFMleft + dat$residFMright)/2
 
-m_behavior_res <- lm(residITD ~ residFMmedian, data = dat) 
-Anova(m_behavior_res)
+m_itd_fm_res <- lm(residITD ~ residFMmedian, data = dat) 
+Anova(m_itd_fm_res)
 
-p_behavior_res <- ggplot(aes(x = residFMmedian, y = residITD), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
-p_behavior_res + xlab('Fdev [residual]') + ylab('ITD [residual]') + annotate("text", x = 5, y = -4, size =8, label = paste("R = ", signif(sqrt(summary(m_behavior_res)$r.squared), 3),  " \nP =",signif(summary(m_behavior_res)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
+p_itd_fm_res <- ggplot(aes(x = residFMmedian, y = residITD), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
+p_itd_fm_res + xlab('Fdev [residual]') + ylab('ITD [residual]') + annotate("text", x = 5, y = -4, size =8, label = paste("R = ", signif(sqrt(summary(m_itd_fm_res)$r.squared), 3),  " \nP =",signif(summary(m_itd_fm_res)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
 #ggtitle('ITD vs FM thresholds (with attention score factored out)')
+
+###########################################################################################################################################################
+#Audiogram
+m_itd_aud500 <- lm(ITDmin ~ HL500diff, data = dat)
+p_itd_aud500 <- ggplot(aes(x = HL500diff, y = ITDmin), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
+p_itd_aud500 + xlab('Audiogram diff @ 500 Hz') + ylab('ITDmin [dB re: 1 us]') + annotate("text", x = 7, y = 32, size =8, label = paste("R = ", signif(sqrt(summary(m_itd_aud500)$r.squared), 3),  " \nP =",signif(summary(m_itd_aud500)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
+
+# Audiogram: high frequnecy average
