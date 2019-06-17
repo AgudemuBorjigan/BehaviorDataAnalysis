34aa60d29cb833588142f73d01f08358bdafb673
agudemuborjigan <agudemubor@gmail.com>
2019-06-17 11:15:48 -0400

    R script updates

--- ITD_FM_FFR.R
+++ ITD_FM_FFR.R
@@ -17,10 +17,16 @@ p_itd_ffr <- ggplot(aes(x = ITDmean, y = FFR1kmagdB), data = dat) + theme_classi
 p_itd_ffr + xlab('ITD [dB re: 1 us]') + ylab('FFR magnitude [dB]') + annotate("text", x = 24, y = 35, size = 8, label = paste("R = ", signif(sqrt(summary(m_itd_ffr)$r.squared), 3),  " \nP =",signif(summary(m_itd_ffr)$coef[2,4], 3), "\n n = 25")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
 #+ ggtitle('ITD threshold vs FFR 1 KHz component')
 
-
+dat$HL500 <- (dat$HL500left + dat$HL500right)/2.0
+dat$HFA <- (dat$HFAleft + dat$HFAright)/2.0
+dat$FMave <- (dat$FMleftMean + dat$FMrightMean)/2.0
 m_itd_ffr_lat = lm(ITDmean ~ mistakeITD + FFR1kmagdB + lat180dB, data = dat, na.action = na.exclude)
-dat$ITDmeanPredicted <- predict(m_itd_ffr_lat)
-#dat$ITDmeanPredicted <- predict(m_itd_lat)
+m_itd_lat = lm(ITDmean ~ mistakeITD + lat180dB, data = dat, na.action = na.exclude)
+m_itd_audiogram_lat = lm(ITDmean ~ mistakeITD + lat180dB + HL500 + HFA, data=dat, na.action = na.exclude)
+# m_itd_audiogram_nomistake_lat = lm(ITDmean ~ lat180dB + HL500 + HFA, data=dat, na.action = na.exclude)
+
+#dat$ITDmeanPredicted <- predict(m_itd_audiogram_lat)
+dat$ITDmeanPredicted <- predict(m_itd_lat)
 m_itd_pred = lm(ITDmean ~ ITDmeanPredicted, data = dat)
 p_itd_pred <- ggplot(aes(x = ITDmeanPredicted, y = ITDmean), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
 p_itd_pred + xlab('Predicted ITD thresholds [dB re: 1 us]') + ylab('ITD thresholds [dB re: 1 us]') + annotate("text", x = 26, y = 35, size = 7, label = paste("R = ", signif(sqrt(summary(m_itd_pred)$r.squared), 3),  " \nP =",signif(summary(m_itd_pred)$coef[2,4], 3), "\n n = 26")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 22))
