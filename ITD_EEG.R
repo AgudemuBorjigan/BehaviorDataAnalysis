120a097d84e6f221fb96fc55f2a5dd532a1fb2ae
agudemuborjigan <agudemubor@gmail.com>
2019-06-17 10:16:37 -0400

    R script updates

--- ITD_EEG.R
+++ ITD_EEG.R
@@ -1,23 +1,63 @@
-dat <- read.csv('~/DataAnalysis/BehaviorDataAnalysis/dataSetBehaviorEEG_ITD.csv', header = TRUE)
+dat <- read.csv('~/Desktop/Lab/Experiment/DataAnalysis/BehaviorDataAnalysis/dataSetBehaviorEEG_ITD.csv', header = TRUE)
 str(dat) # structure of dat
 # library for linear regression modeling
 library(lme4)
 library(car) 
 library(ggplot2)
 
-dat$FM <- (dat$FMleft + dat$FMright)/2
+dat$FMmean <- (dat$FMleftMean + dat$FMrightMean)/2
+dat$HL500 <- (dat$HL500left + dat$HL500right)/2
+dat$HL500diff <- abs(dat$HL500left - dat$HL500right)
+dat$HFA <- (dat$HFAleft + dat$HFAright)/2
+dat$HFAdiff <- abs(dat$HFAleft - dat$HFAright)
 
-m_itd_res <- lm(ITD ~ mistakeITD, data = dat)
-dat$residITD <- resid(m_itd_res) # adding residuals of the parameter to the data set, with the effect of certain factor(s) removed
+
+m_itdMean_res <- lm(ITDmean ~ mistakeITD, data = dat)
+dat$residITDmean <- resid(m_itdMean_res) # adding residuals of the parameter to the data set, with the effect of certain factor(s) removed
+
+m_fmLeft_res <- lm(FMleftMean ~ mistakeFMLeft, data = dat)
+dat$residFMleft <- resid(m_fmLeft_res) # adding residuals of the parameter to the data set, with the effect of certain factor(s) removed
+
+m_fmRight_res <- lm(FMrightMean ~ mistakeFMRight, data = dat)
+dat$residFMright <- resid(m_fmRight_res) # adding residuals of the parameter to the data set, with the effect of certain factor(s) removed
+
+dat$residFMmean <- (dat$residFMleft + dat$residFMright)/2
+
 
 dat$lat180 <- (dat$ERPn1lat180 + dat$ERPp2lat180)/2
 dat$lat180dB <- 10 * log10(dat$ERPn1lat180/0.1) + 10 * log10(dat$ERPp2lat180/0.2)
-m_itd_lat <- lm(ITD ~ lat180dB, data = dat)
+dat$ERPmag180dB <- 20 * log10(dat$ERPmag180)
+dat$ITCmag180dB <- 20 * log10(dat$ITCmag180)
 
-p_itd_lat <- ggplot(aes(x = ITD, y = lat180dB), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
-p_itd_lat + xlab('ITD [dB re: 1 us]') + ylab('Latency [dB re: 1 ms]\ncond: ITD = 180 us') + annotate("text", x = 24, y = 2, size = 8, label = paste("R = ", signif(sqrt(summary(m_itd_lat)$r.squared), 3),  " \nP =",signif(summary(m_itd_lat)$coef[2,4], 3), "\n n = 32")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
+# latencies
+m_itdMean_lat <- lm(ITDmean ~ lat180dB, data = dat)
+p_itdMean_lat <- ggplot(aes(x = ITDmean, y = lat180dB), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
+p_itdMean_lat + xlab('ITDmean [dB re: 1 us]') + ylab('Latency [dB re: 1 ms]\ncond: ITD = 180 us') + annotate("text", x = 24, y = 2, size = 8, label = paste("R = ", signif(sqrt(summary(m_itdMean_lat)$r.squared), 3),  " \nP =",signif(summary(m_itdMean_lat)$coef[2,4], 3), "\n n = 32")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
 # + ggtitle('ITD threshold vs average of n1 and p2 latencies')
 
-m_ITDresid_lat <- lm(residITD ~ lat180dB, data = dat)
-p_ITDresid_lat <- ggplot(aes(x = residITD, y = lat180dB), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
-p_ITDresid_lat + xlab('ITD [residual]') + ylab('Latency [dB re: 1 ms]\ncond: ITD = 180 us') + annotate("text", x = -4, y = 2.1, size = 8, label = paste("R = ", signif(sqrt(summary(m_ITDresid_lat)$r.squared), 3),  " \nP =",signif(summary(m_ITDresid_lat)$coef[2,4], 3), "\n n = 32")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
+m_ITDmeanResid_lat <- lm(residITDmean ~ lat180dB, data = dat)
+p_ITDmeanResid_lat <- ggplot(aes(x = residITDmean, y = lat180dB), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
+p_ITDmeanResid_lat + xlab('ITD [residual]') + ylab('Latency [dB re: 1 ms]\ncond: ITD = 180 us') + annotate("text", x = -4, y = 2.1, size = 8, label = paste("R = ", signif(sqrt(summary(m_ITDmeanResid_lat)$r.squared), 3),  " \nP =",signif(summary(m_ITDmeanResid_lat)$coef[2,4], 3), "\n n = 32")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
+
+m_FMmeanResid_lat <- lm(residFMmean ~ lat180dB, data = dat)
+m_FMmean_lat <- lm(FMmean ~ lat180dB, data = dat)
+m_aud500_lat <- lm(HL500 ~ lat180dB, data = dat)
+m_audHFA_lat <- lm(HFA ~ lat180dB, data = dat)
+
+# magnitudes
+m_ITDmean_mag_erp <- lm(ITDmean ~ ERPmag180dB, data = dat)
+m_ITDmean_mag_itc <- lm(ITDmean ~ ITCmag180dB, data = dat)
+m_ITDmeanResid_mag_erp <- lm(residITDmean ~ ERPmag180dB, data = dat)
+m_ITDmeanResid_mag_itc <- lm(residITDmean ~ ITCmag180dB, data = dat)
+
+m_FMmean_mag_erp <- lm(FMmean ~ ERPmag180dB, data = dat)
+m_FMmean_mag_itc <- lm(FMmean ~ ITCmag180dB, data = dat)
+m_FMmeanResid_mag_erp <- lm(residFMmean ~ ERPmag180dB, data = dat)
+m_FMmeanResid_mag_itc <- lm(residFMmean ~ ITCmag180dB, data = dat)
+
+m_audHFA_mag_erp <- lm(HFA ~ ERPmag180, data = dat)
+p_audHFA_mag_erp <- ggplot(aes(x = HFA, y = ERPmag180), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
+p_audHFA_mag_erp + xlab('HFA') + ylab('ERP magnitude \ncond: ITD = 180 us') + annotate("text", x = -2, y = 3, size = 8, label = paste("R = ", signif(sqrt(summary(m_audHFA_mag_erp)$r.squared), 3),  " \nP =",signif(summary(m_audHFA_mag_erp)$coef[2,4], 3), "\n n = 32")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
+m_audHFA_mag_itc <- lm(HFA ~ ITCmag180dB, data = dat)
+m_aud500_mag_erp <- lm(HL500 ~ ERPmag180dB, data = dat)
+m_aud500_mag_itc <- lm(HL500 ~ ITCmag180dB, data = dat)
--- ITD_FM.R
+++ ITD_FM.R
@@ -1,6 +1,6 @@
 #setwd('~/DataAnalysis/BehaviorDataAnalysis/')
 #dat <- read.csv('dataSetBehavior.csv', header = TRUE)
-dat <- read.csv('~/DataAnalysis/BehaviorDataAnalysis/dataSetBehavior.csv', header = TRUE)
+dat <- read.csv('~/Desktop/Lab/Experiment/DataAnalysis/BehaviorDataAnalysis/dataSetBehavior.csv', header = TRUE)
 str(dat) # structure of dat
 # library for linear regression modeling
 library(lme4)
@@ -21,7 +21,7 @@ m_itd_fm <- lm(ITDmax ~ FMmedian, data = dat)
 Anova(m_itd_fm)
 
 p_itd_fm <- ggplot(aes(x = FMmedian, y = ITDmax), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
-p_itd_fm + xlab('Fdev [dB re: 500 Hz]') + ylab('ITD [dB re: 1 us]') + annotate("text", x = 12, y = 44.5, size =8, label = paste("R = ", signif(sqrt(summary(m_itd_fm)$r.squared), 3),  " \nP =",signif(summary(m_itd_fm)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
+p_itd_fm + xlab('Fdev [dB re: 500 Hz]') + ylab('ITD [dB re: 1 us]') + annotate("text", x = 12, y = 44.5, size =8, label = paste("R = ", signif(sqrt(summary(m_itd_fm)$r.squared), 3),  " \nP =", signif(summary(m_itd_fm)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
 #ggtitle('ITD vs FM thresholds')
 
 m_itd_res <- lm(ITDmax ~ mistakeITD, data = dat)
@@ -39,13 +39,21 @@ m_itd_fm_res <- lm(residITD ~ residFMmedian, data = dat)
 Anova(m_itd_fm_res)
 
 p_itd_fm_res <- ggplot(aes(x = residFMmedian, y = residITD), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
-p_itd_fm_res + xlab('Fdev [residual]') + ylab('ITD [residual]') + annotate("text", x = 5, y = -4, size =8, label = paste("R = ", signif(sqrt(summary(m_itd_fm_res)$r.squared), 3),  " \nP =",signif(summary(m_itd_fm_res)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
+p_itd_fm_res + xlab('Fdev [residual]') + ylab('ITD [residual]') + annotate("text", x = 5, y = -4, size =8, label = paste("R = ", signif(sqrt(summary(m_itd_fm_res)$r.squared), 3),  " \nP =", signif(summary(m_itd_fm_res)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
 #ggtitle('ITD vs FM thresholds (with attention score factored out)')
 
 ###########################################################################################################################################################
-#Audiogram
+#ITD and audiogram
 m_itd_aud500 <- lm(ITDmin ~ HL500diff, data = dat)
 p_itd_aud500 <- ggplot(aes(x = HL500diff, y = ITDmin), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
 p_itd_aud500 + xlab('Audiogram diff @ 500 Hz') + ylab('ITDmin [dB re: 1 us]') + annotate("text", x = 7, y = 32, size =8, label = paste("R = ", signif(sqrt(summary(m_itd_aud500)$r.squared), 3),  " \nP =",signif(summary(m_itd_aud500)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
 
-# Audiogram: high frequnecy average
+#ITD and audiogram: high frequnecy average
+m_itd_audHFA <- lm(ITDmin ~ HFAdiff, data = dat)
+p_itd_audHFA <- ggplot(aes(x = HFAdiff, y = ITDmin), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
+p_itd_audHFA + xlab('Audiogram HFA, diff') + ylab('ITDmin [dB re: 1 us]') + annotate("text", x = 9, y = 32, size =8, label = paste("R = ", signif(sqrt(summary(m_itd_audHFA)$r.squared), 3),  " \nP =",signif(summary(m_itd_audHFA)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
+
+#FM and audiogram
+m_fm_aud500 <- lm(FMrightMean ~ HL500, data = dat)
+p_fm_aud500 <- ggplot(aes(x = HL500, y = FMrightMean), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
+p_fm_aud500 + xlab('Audiogram avg @ 500 Hz') + ylab('FMrightMean [dB re: 1 Hz]') + annotate("text", x = -2, y = 20, size =8, label = paste("R = ", signif(sqrt(summary(m_fm_aud500)$r.squared), 3),  " \nP =",signif(summary(m_fm_aud500)$coef[2,4], 3), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 24))
