f4fefe2fbd8bc4f4d18e5e8c6b85977fcbf5076e
agudemuborjigan <agudemubor@gmail.com>
2019-09-06 10:40:35 -0400

    minor

--- ITD_EEG.R
+++ ITD_EEG.R
@@ -32,12 +32,12 @@ dat$ITCmag180dB <- 20 * log10(dat$ITCmag180)
 # latencies
 m_itdMean_lat <- lm(ITDmean ~ lat180dB, data = dat)
 p_itdMean_lat <- ggplot(aes(x = ITDmean, y = lat180dB), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
-p_itdMean_lat + xlab('ITDmean [dB re: 1 us]') + ylab('Latency [dB re: 1 ms]\ncond: ITD = 180 us') + annotate("text", x = 24, y = 2, size = 8, label = paste("R = ", signif(sqrt(summary(m_itdMean_lat)$r.squared), 3),  " \nP =",signif(summary(m_itdMean_lat)$coef[2,4], 3), "\n n = 32")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
+p_itdMean_lat + xlab('ITDmean [dB re: 1 us]') + ylab('Latency [dB re: 1 ms]\ncond: ITD = 180 us') + annotate("text", x = 24, y = 2, size = 8, label = paste("R = ", signif(sqrt(summary(m_itdMean_lat)$r.squared), 3),  " \nP =",signif(summary(m_itdMean_lat)$coef[2,4], 3), "\n n = 31")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
 # + ggtitle('ITD threshold vs average of n1 and p2 latencies')
 
 m_ITDmeanResid_lat <- lm(residITDmean ~ lat180dB, data = dat)
 p_ITDmeanResid_lat <- ggplot(aes(x = residITDmean, y = lat180dB), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
-p_ITDmeanResid_lat + xlab('ITD [residual]') + ylab('Latency [dB re: 1 ms]\ncond: ITD = 180 us') + annotate("text", x = -4, y = 2.1, size = 8, label = paste("R = ", signif(sqrt(summary(m_ITDmeanResid_lat)$r.squared), 3),  " \nP =",signif(summary(m_ITDmeanResid_lat)$coef[2,4], 3), "\n n = 32")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
+p_ITDmeanResid_lat + xlab('ITD [residual]') + ylab('Latency [dB re: 1 ms]\ncond: ITD = 180 us') + annotate("text", x = -4, y = 2.1, size = 8, label = paste("R = ", signif(sqrt(summary(m_ITDmeanResid_lat)$r.squared), 3),  " \nP =",signif(summary(m_ITDmeanResid_lat)$coef[2,4], 3), "\n n = 31")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
 
 m_FMmeanResid_lat <- lm(residFMmean ~ lat180dB, data = dat)
 m_FMmean_lat <- lm(FMmean ~ lat180dB, data = dat)
--- ITD_FM_FFR.R
+++ ITD_FM_FFR.R
@@ -1,33 +1,48 @@
-dat <- read.csv('~/Desktop/Lab/Experiment/DataAnalysis/BehaviorDataAnalysis/dataSetBehaviorEEG_ITD_FFR.csv', header = TRUE)
+dat <- read.csv('~/Desktop/Lab/Experiment/DataAnalysis/BehaviorDataAnalysis/dataSet.csv', header = TRUE)
 str(dat) # structure of dat
 # library for linear regression modeling
 library(lme4)
 library(car) 
 library(ggplot2)
 
-dat$ABR <- (dat$ABRneg + dat$ABRpos)/2
-dat$FFR1Kmag_nor <- 20 * log10(dat$FFR1Kmag / dat$ABR)
-dat$FFR1kmagdB <- 20 * log10(dat$FFR1Kmag/1e-6)
+dat$ref_original <- dat$ref * 10e-6
+dat$magMaskerSum_nor <- 20 * log10(dat$magMaskerSum / dat$ref_original)
+dat$magMaskerSum_dB <- 20 * log10(dat$magMaskerSum/1e-6)
+dat$magAdptDiff_nor <- 20 * log10(dat$magAdptDiff / dat$ref_original)
+dat$magMaskerDiff_nor <- 20 * log10(dat$magMaskerDiff / dat$ref_original)
+dat$magProbDiff_nor <- 20 * log10(dat$magProbDiff / dat$ref_original)
 dat$lat180dB <- 10 * log10(dat$ERPn1lat180/0.1) + 10 * log10(dat$ERPp2lat180/0.2)
-
-m_itd_ffr_nor = lm(ITDmean ~ FFR1Kmag_nor, data = dat, na.action = na.exclude)
-m_itd_lat = lm(ITDmean ~ mistakeITD + lat180dB, data = dat, na.action = na.exclude)
-m_itd_ffr = lm(ITDmean ~ FFR1kmagdB, data = dat, na.action = na.exclude)
-p_itd_ffr <- ggplot(aes(x = ITDmean, y = FFR1kmagdB), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
-p_itd_ffr + xlab('ITD [dB re: 1 us]') + ylab('FFR magnitude [dB]') + annotate("text", x = 24, y = 35, size = 8, label = paste("R = ", signif(sqrt(summary(m_itd_ffr)$r.squared), 3),  " \nP =",signif(summary(m_itd_ffr)$coef[2,4], 3), "\n n = 25")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25))
-#+ ggtitle('ITD threshold vs FFR 1 KHz component')
-
 dat$HL500 <- (dat$HL500left + dat$HL500right)/2.0
 dat$HFA <- (dat$HFAleft + dat$HFAright)/2.0
-dat$FMave <- (dat$FMleftMean + dat$FMrightMean)/2.0
-m_itd_ffr_lat = lm(ITDmean ~ mistakeITD + FFR1kmagdB + lat180dB, data = dat, na.action = na.exclude)
-m_itd_lat = lm(ITDmean ~ mistakeITD + lat180dB, data = dat, na.action = na.exclude)
-m_itd_audiogram_lat = lm(ITDmean ~ mistakeITD + lat180dB + HL500 + HFA, data=dat, na.action = na.exclude)
-# m_itd_audiogram_nomistake_lat = lm(ITDmean ~ lat180dB + HL500 + HFA, data=dat, na.action = na.exclude)
+dat$FMmean <- (dat$FMleftMean + dat$FMrightMean)/2.0
+dat$FMmedian <- (dat$FMleftMedian + dat$FMrightMedian)/2.0
+dat$FMmax <- (dat$FMleftMax + dat$FMrightMax)/2.0
+
+m_itd_res <- lm(ITDmean ~ mistakeITD, data = dat)
+m_fmLeft_res <- lm(FMleftMean ~ mistakeFMLeft, data = dat)
+m_fmRight_res <- lm(FMrightMean ~ mistakeFMRight, data = dat)
+dat$residITDmean <- resid(m_itd_res)
+dat$residFMright <- resid(m_fmRight_res)
+dat$residFMleft <- resid(m_fmLeft_res)
+dat$residFMmean <- (dat$residFMleft + dat$residFMright)/2
+
+m_all = lm(ITDmean ~ mistakeITD + lat180dB + HL500 + HFA + magMaskerSum_nor, data = dat, na.action = na.exclude)
+m = lm(ITDmean ~ FMmean, data = dat, na.action = na.exclude)
+
+model = m
+Y = dat$ITDmean
+X = dat$FMmean
+#which(dat$lat180dB %in% NaN) # it tells you the index of 'NaN'
+ylabel = 'ITD detection thresholds \n [dB re: 1 us]'
+xlabel = 'FM detection thresholds [dB re: 1 Hz]'
+title = 'ITD thresholds vs FM thresholds'
+#FM residual [dB re: 500 Hz], Latency [dB re: 1 ms], \ncond: ITD = 180 us, ITD detection thresholds\n[residual], FM detection thresholds\n[residual]
+p <- ggplot(aes(x = X, y = Y), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
+p + xlab(xlabel) + ylab(ylabel) + annotate("text", x = 20, y = 25, size = 8, label = paste("R = ", signif(sqrt(summary(model)$r.squared), 1),  " \nP =",signif(summary(model)$coef[2,4], 1), "\n n = 33")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 25)) + ggtitle(title) 
+#+ xlim(20, 37)
 
 #dat$ITDmeanPredicted <- predict(m_itd_audiogram_lat)
-dat$ITDmeanPredicted <- predict(m_itd_lat)
+dat$ITDmeanPredicted <- predict(m)
 m_itd_pred = lm(ITDmean ~ ITDmeanPredicted, data = dat)
 p_itd_pred <- ggplot(aes(x = ITDmeanPredicted, y = ITDmean), data = dat) + theme_classic() + geom_point(size = 3) + geom_smooth(method = "lm", col = "red", se = TRUE) 
-p_itd_pred + xlab('Predicted ITD thresholds [dB re: 1 us]') + ylab('ITD thresholds [dB re: 1 us]') + annotate("text", x = 26, y = 35, size = 7, label = paste("R = ", signif(sqrt(summary(m_itd_pred)$r.squared), 3),  " \nP =",signif(summary(m_itd_pred)$coef[2,4], 3), "\n n = 26")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 22))
-#ggtitle('Predicted vs actual ITD thresholds')
+p_itd_pred + xlab('Predicted ITD thresholds [dB re: 1 us]') + ylab('ITD thresholds [dB re: 1 us]') + annotate("text", x = 26, y = 36, size = 7, label = paste("R = ", signif(sqrt(summary(m_itd_pred)$r.squared), 1),  " \nP =",signif(summary(m_itd_pred)$coef[2,4], 1), "\n n = 31")) + theme_update(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 22)) + ggtitle('Predicted vs actual ITD thresholds')
